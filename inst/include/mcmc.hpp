48ba37b6d0e0f708bf6881e53e1982f68dcf5c92
Tendayi Kamucheka <ftendayi@gmail.com>
2021-09-02 09:53:01 -0500

    Updates function parameters to match function calls

--- inst/include/mcmc.hpp
+++ inst/include/mcmc.hpp
@@ -24,27 +24,51 @@
 static resamplers_t Resamplers;
 static distributions_t Distributions;
 
-void generateInput(Eigen::VectorXd *prior_x_t, Eigen::VectorXd *y_t,
-                   Eigen::MatrixXd &F, Eigen::MatrixXd &G, Eigen::MatrixXd &I_1,
-                   Eigen::MatrixXd &I_2, dim_t N, dim_t d, dim_t timeSteps);
-void initialize(std::string distribution_opt, Eigen::VectorXd **x_t, Eigen::VectorXd *w_t,
-                Eigen::VectorXd m0, Eigen::MatrixXd C0,
-                unsigned N, unsigned d, unsigned t, float df);
-void propagate_K(std::string distribution_opt, Eigen::VectorXd **post_x_t, unsigned *a_t,
-                 const Eigen::MatrixXd Q, const dim_t N, const dim_t d,
-                 const dim_t t, const float df);
-void reweight_G(std::string distribution_opt, Eigen::VectorXd *w_t, const Eigen::VectorXd *y_t,
-                Eigen::VectorXd **post_x_t, const double norm,
-                const Eigen::MatrixXd &E_inv, const Eigen::MatrixXd E, const Eigen::MatrixXd F,
-                const dim_t N, const dim_t d, const dim_t t, const float df);
-void MCMC(Eigen::VectorXd **post_x_t, Eigen::VectorXd *w_t, unsigned *a_t,
-          Eigen::VectorXd *y_t, Eigen::MatrixXd &F, Eigen::MatrixXd &I,
-          const dim_t N, const dim_t d, const dim_t timeSteps,
-          std::string resampler_opt, std::string distribution_opt, const float df);
+void generateInput(
+    Eigen::VectorXd *prior_x_t, Eigen::VectorXd *y_t,
+    Eigen::MatrixXd &F, Eigen::MatrixXd &G, Eigen::MatrixXd &I_1, Eigen::MatrixXd &I_2,
+    dim_t N, dim_t d, dim_t timeSteps);
 
-void MCMC_step(Eigen::VectorXd **post_x_t, Eigen::VectorXd *w_t, unsigned *a_t,
-               Eigen::VectorXd *y_t, Eigen::MatrixXd &F, Eigen::MatrixXd &I,
-               const dim_t N, const dim_t d, const dim_t timeSteps,
-               std::string resampler_opt, std::string distribution_opt, const float df);
+void initialize(
+    Eigen::VectorXd **x_t, Eigen::VectorXd *w_t,
+    Eigen::VectorXd m0, Eigen::MatrixXd C0,
+    unsigned N, unsigned d, unsigned t, float df, std::string distribution_opt);
+
+void propagate_K(
+    Eigen::VectorXd **post_x_t, unsigned *a_t,
+    const Eigen::MatrixXd G, const Eigen::MatrixXd Q_w,
+    const dim_t N, const dim_t d, const dim_t t,
+    const float df,
+    std::string distribution_opt);
+
+void reweight_G(
+    Eigen::VectorXd *w_t, const Eigen::VectorXd *y_t,
+    Eigen::VectorXd **post_x_t,
+    const Eigen::MatrixXd &F,
+    const Eigen::MatrixXd &V_det,
+    const Eigen::MatrixXd &V_inv,
+    const dim_t N, const dim_t d, const dim_t t,
+    const float df,
+    std::string distributions_opt);
+
+void MCMC(
+    Eigen::VectorXd **post_x_t,
+    Eigen::VectorXd *w_t, unsigned *a_t,
+    Eigen::VectorXd *y_t,
+    const Eigen::MatrixXd &F, const Eigen::MatrixXd &G,
+    const Eigen::matrixXd &V, const Eigen::MatrixXd &W,
+    const dim_t N, const dim_t d, const dim_t timeSteps, const float df,
+    std::string resampler_opt,
+    std::string distribution_opt);
+
+void MCMC_step(
+    Eigen::VectorXd **post_x_t,
+    Eigen::VectorXd *w_t, unsigned *a_t,
+    Eigen::VectorXd *y_t,
+    const Eigen::MatrixXd &F, const Eigen::MatrixXd &G,
+    const Eigen::matrixXd &V, const Eigen::MatrixXd &W,
+    const dim_t N, const dim_t d, const dim_t timeSteps, const float df,
+    std::string resampler_opt,
+    std::string distribution_opt);
 
 #endif
